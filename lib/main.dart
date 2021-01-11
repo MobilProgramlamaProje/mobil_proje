0fd4fb625c07fd5de83fed7ca72aaea42baa8456
İlker DOĞAN <ilkerdogan012327@gmail.com>
2021-01-11 19:44:22 +0300

    Uygulamamızın ilk açılış ekranı yeniden yapıldı. 3 adet button konulup, butonların hangi sayfaya yönlendireceği belirlendi.

--- lib/main.dart
+++ lib/main.dart
@@ -1,132 +1,147 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:mobil_proje/KayitGiris/kurye_giris.dart';
+import 'package:mobil_proje/KayitGiris/musteri_giris.dart';
+import 'package:mobil_proje/KayitGiris/yonetici_giris.dart';
 
 void main() {
   runApp(MaterialApp(
-    initialRoute: '/',
-    routes: {
-      '/': (context) => HomeRoute(),
-      '/second': (context) => SecondRoute(),
-      '/third': (context) => ThirdRoute(),
-      '/fourth': (context) => FourthRoute(),
-    },
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
   ));
 }
 
-class HomeRoute extends StatelessWidget {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: new ThemeData( primarySwatch: Colors.red, ),
-      home: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            toolbarHeight: 60,
-            title: Text('SüperMarketim'),
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          // we will give media query height
+          // double.infinity make it big as my parent allows
+          // while MediaQuery make it big as per the screen
+
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+          child: Column(
+            // even space distribution
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Text(
+                    "Süper Marketim",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue[700],
+                      fontSize: 40,
+
+                    ),
+
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text("Alışverişin En Güvenlisi Ve En Hızlısı ",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.blueAccent[700],
+                      fontSize: 20,
+
+                    ),)
+                ],
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height / 3,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/shop.jpg")
+                    )
+                ),
+              ),
+
+              Column(
+                children: <Widget>[
+                  // the login button
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => MusteriGiris()));
+
+                    },
+                    // defining the shape
+                    shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                            color: Colors.black
+                        ),
+                        borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: Text(
+                      "Müşteri",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18
+                      ),
+                    ),
+                  ),
+                  // creating the signup button
+                  SizedBox(height:20),
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => YoneticiGiris()));
+
+                    },
+                    // defining the shape
+                    shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                            color: Colors.black
+                        ),
+                        borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: Text(
+                      "Yönetici",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18
+                      ),
+                    ),
+                  ),
+
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => KuryeGiris()));
+
+                    },
+                    // defining the shape
+                    shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                            color: Colors.black
+                        ),
+                        borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: Text(
+                      "Kurye",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18
+                      ),
+                    ),
+                  ),
+                ],
+              )
+
+
+
+            ],
           ),
-          backgroundColor: Colors.grey,
-          body: Center( child: Column( children: <Widget>[
-            Container(
-              margin: EdgeInsets.all(50),
-              width: 250,
-              height: 55,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [Colors.red, Colors.blue],
-                  begin: FractionalOffset.centerLeft,
-                  end: FractionalOffset.centerRight,
-                ),
-              ),
-
-              child : FlatButton(
-                child: Text('Yönetici',  style: TextStyle(color: Colors.lime[50]),),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/second');
-                },
-              ),
-            ),
-
-            Container(
-              margin: EdgeInsets.all(50),
-              width: 250,
-              height: 55,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [Colors.red, Colors.blue],
-                  begin: FractionalOffset.centerLeft,
-                  end: FractionalOffset.centerRight,
-                ),
-              ),
-
-
-
-              child : FlatButton(
-                child: Text('Kurye',  style: TextStyle(color: Colors.lime[50]),),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/third');
-                },
-              ),
-            ),
-
-            Container(
-              margin: EdgeInsets.all(50),
-              width: 250,
-              height: 55,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [Colors.red, Colors.blue],
-                  begin: FractionalOffset.centerLeft,
-                  end: FractionalOffset.centerRight,
-                ),
-              ),
-
-              child : FlatButton(
-                child: Text('Kullanıcı',  style: TextStyle(color: Colors.lime[50]),),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/fourth');
-                },
-              ),
-            ),
-          ],
-
-          ))),
-    );
-  }
-}
-
-class SecondRoute extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("YÖNETİCİ "),
-        backgroundColor: Colors.purple[900],
-      ),
-    );
-  }
-}
-
-class ThirdRoute extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("KURYE"),
-        backgroundColor: Colors.purple[700],
-      ),
-    );
-  }
-}
-
-
-
-class FourthRoute extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("KULLANICI"),
-        backgroundColor: Colors.purple[500],
+        ),
       ),
     );
   }
