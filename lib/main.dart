f167f3a65b37cae6ae4931d6d300b850f42c948e
melike yıldırım <melikeyildirim718@gmail.com>
2021-01-12 00:35:13 +0300

    Frebase' i projemize ekledim.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'dart:ui';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mobil_proje/KayitGiris/kurye_giris.dart';
 import 'package:mobil_proje/KayitGiris/musteri_giris.dart';
 import 'package:mobil_proje/KayitGiris/yonetici_giris.dart';
+
+import 'login_islemleri.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -143,6 +146,41 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class App extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(
+              child: Text("Hata cıktı" + snapshot.error.toString()),
+            )
+          );
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return LoginIslemleri();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Scaffold(
+            body: Center(
+            child: CircularProgressIndicator(),
+        )
+        );
+      },
     );
   }
 }
