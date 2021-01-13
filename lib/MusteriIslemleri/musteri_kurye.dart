433b39b323200a867fd08ab29982ddb7dadedb29
İlker DOĞAN <ilkerdogan012327@gmail.com>
2021-01-13 15:33:27 +0300

    Müşteri giriş yaptığında karşılacağı sayfa ve hangi işlemleri yapacağı belirlendi. Bu sayfalar için MusteriIslemleri adlı bir klasör açılıp, bu klasörün içine gerekli dart sayfaları oluşturuldu. Bu ekranların dizaynı yapıldı, kod blokları oluşturuldu.

---
+++ lib/MusteriIslemleri/musteri_kurye.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MusteriKurye extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.green[700],
+        appBar: AppBar(
+          title: Text('Ürün Ekranı'),
+          centerTitle: true,
+          backgroundColor: Colors.grey[850],
+          elevation: 0.0,
+
+        )
+
+
+
+    );
+  }
+}
