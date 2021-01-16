744c99b39f238c5e5524f6c1e54695e49fc6bd8f
Metin Mert <mertinmerte@outlook.com>
2021-01-17 00:38:11 +0300

    Müşteri Kayıt ekranı ve Kurye Ana Ekranı Interface'i güncellendi. Arayüze fotoğraflar eklendi ve güncellendi. "kurye_sirapis_ekrani.dart" dosyası silindi.

--- lib/KuryeIslemleri/kurye_ana_ekran.dart
+++ lib/KuryeIslemleri/kurye_ana_ekran.dart
@@ -1,37 +1,118 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'file:///C:/Users/Acer/Desktop/mobil_proje/lib/KuryeIslemleri/kurye_sirapis_ekrani.dart';
+import 'package:mobil_proje/KuryeIslemleri/kurye_guncel_siparis.dart';
+import 'package:mobil_proje/KuryeIslemleri/kurye_siparis_gecmisi.dart';
+import 'kurye_siparis_gecmisi.dart';
 
 class KuryeAnaEkran extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Siparislerim',
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery
+              .of(context)
+              .size
+              .height,
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+          child: Column(
+            // even space distribution
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Text(
+                    "Süper Marketim",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue[700],
+                      fontSize: 32,
+
+                    ),
+
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text("",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.blueAccent[700],
+                      fontSize: 18,
+
+                    ),)
+                ],
+              ),
+              Container(
+                height: MediaQuery
+                    .of(context)
+                    .size
+                    .height / 3,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/map.png")
+                    )
+                ),
+              ),
+
+              Column(
+                children: <Widget>[
+                  // the login button
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(
+                          builder: (context) => KuryeSiparisGecmisi()));
+                    },
+                    // defining the shape
+                    shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                            color: Colors.black
+                        ),
+                        borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: Text(
+                      "Geçmiş Siparişlerim",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18
+                      ),
+                    ),
+                  ),
+                  // creating the signup button
+                  SizedBox(height: 20),
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(
+                          builder: (context) => KuryeGuncelSiparis()));
+                    },
+                    // defining the shape
+                    shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                            color: Colors.black
+                        ),
+                        borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: Text(
+                      "Güncel Siparişim",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                ],
+              )
+            ],
+          ),
         ),
       ),
-      body: _buildListView(context),
-    );
-  }
-
-  ListView _buildListView(BuildContext context) {
-    return ListView.builder(
-      itemCount: 10,
-      itemBuilder: (_, index) {
-        return Card(
-        child: ListTile(
-          title: Text('Adres Cadde Sokak vb.'),
-          subtitle: Text('$index. Sipariş '),
-          leading: Icon(Icons.add_location_sharp),
-          trailing: IconButton(
-          icon: Icon(Icons.arrow_forward_ios_outlined),
-          onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context) => KuryeSiparisEkrani(index)));
-          },
-          ),
-          ),
-        );
-      },
     );
   }
 }
---
+++ lib/KuryeIslemleri/kurye_guncel_siparis.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class KuryeGuncelSiparis extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Adres:'),
+      ),
+      body: Center(
+      ),
+    );
+  }
+}
+
